8ae52c99245bda9419041ad8440bd91716d5f1d6
Adam Boley <89386152+AdamBoley@users.noreply.github.com>
2023-01-22 21:12:11 +0000

    fix issues with html, css and js files

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 // DOM content loaded event listener, delays activation of JS functions until DOM loaded
 document.addEventListener("DOMContentLoaded", function(){
-    let firstNextButton = document.getElementById("first-next");
-    firstNextButton.addEventListener('click', question1);
+    console.log("DOM Loaded")
+    document.getElementById("call-function-1").addEventListener('click', question1);
 })
 
 // Global-scoped object that holds data about a user
@@ -12,8 +12,7 @@ document.addEventListener("DOMContentLoaded", function(){
 // update smoke, alcohol, etc to use Boolean True/False values instead of string values
 let userData = {
     amountToSave: 0,
-    timeScale: 0,
-    timeUnits: "months",
+    time: 0,
     currency: "pounds",
     smoke: false,
     smokeAmount: 0,
@@ -27,28 +26,23 @@ let userData = {
     lotteryAmount: 0,
 }
 
-// progress bar must not wrapped inside any question element
-// progress bar has ID of progress-bar
-// progres-bar fill is controlled by style.width
-// set style.width to various increasing percentage values
-let progressBar = document.getElementById('progress-bar').style.width
-
 // declare
 let currency = document.getElementById('currency') // grab user input of currency, will use in API
-let question1 = document.getElementById('question1-amount') // amount to save question wrapper div
-let question2 = document.getElementById('question2-time') // amount of time question wrapper div
-let question3 = document.getElementById('question3-smoke') // smoke question wrapper div
-let question4 = document.getElementById('question4-alcohol') // alcohol div wrapper
-let question5 = document.getElementById('question5-coffee') // coffee question wrapper div
-let question6 = document.getElementById('question6-clothing') // clothing question wrapper div
-let question7 = document.getElementById('question7-lottery') // lottery question wrapper div
+let question_1 = document.getElementById('question1-amount') // amount to save question wrapper div
+let question_2 = document.getElementById('question2-time') // amount of time question wrapper div
+let question_3 = document.getElementById('question3-smoke') // smoke question wrapper div
+let question_4 = document.getElementById('question4-alcohol') // alcohol div wrapper
+let question_5 = document.getElementById('question5-coffee') // coffee question wrapper div
+let question_6 = document.getElementById('question6-clothing') // clothing question wrapper div
+let question_7 = document.getElementById('question7-lottery') // lottery question wrapper div
 let finalOutput = document.getElementById('final-output') // final output wrapper div
 
 // question 1 - relates to wireframe question-1.png
 // question-1 HTML is displayed by default
 function question1() {
 
-    question1.style.display = 'block'
+    // question1.style.display = 'block'
+    // don't need because questio1 already displays
 
     let currency = document.getElementById('currency').value //point to currency input element
     let amount = document.getElementById('amount').value // points to amount input element
@@ -57,6 +51,7 @@ function question1() {
     console.log(`amount is ${amount}`)
 
     // update progress-bar length to 0%
+    var progressBar = document.getElementById('progress-bar').style.width
     progressBar = '0%'
     
     // update userData object and log value to console
@@ -64,6 +59,7 @@ function question1() {
     userData['amountToSave'] = amount
     console.log(`userData object has values: ${userData}`)
 
+    question_1.style.display = 'none'
     // call next function
     question2();
 }
@@ -72,23 +68,20 @@ function question1() {
 function question2() {
 
     // hide question1 element, show question2 element
-    question1.style.display = 'none'
-    question2.style.display = 'block'
+    question_1.style.display = 'none'
+    question_2.style.display = 'block'
 
     // variable assignment
     let time = document.getElementById('time').value //points to time input element
-    let timeUnits = document.getElementById('time-units').value // points to time-unit input element
 
     // log out values || Remove these in final build
     console.log(`time value is ${time}`)
-    console.log(`units of time are ${timeUnits}`)
 
     // update progress-bar length to 14%
     progressBar = '14%'
 
     // update userData object and log value to console
-    userData['timeScale'] = time
-    userData['timeUnits'] = timeUnits
+    userData['time'] = time
 
     // log out userData object
     console.log(`userData object has values: ${userData}`)
@@ -100,15 +93,15 @@ function question2() {
 // question 3 - relates to question-3.png, grabs smoke amount
 function question3() {
     
-    question2.style.display = 'none'
-    question3.style.display = 'block'
+    question_2.style.display = 'none'
+    question_3.style.display = 'block'
 
-    let smokeRadioSelect = document.getElementById('smoke-radio').value
+    let smokeRadioSelect = document.getElementsByName('smoke-radio').value
     // here we need to see what the user has selected.
     // by default, the "No" radio button is selected, so the amount of cigarettes radio selet form is hidden by the CSS
     // if the user selects "Yes", the amount of cigarettes form should show
     // radio buttons are input elements with type=radio, and can accept a value attribute, so we should be able to grab its value
-    let smokeAmountRadioForm = document.getElementById('smoke-amount')
+    let smokeAmountRadioForm = document.getElementById('smoke-amount-div')
 
     // update progress-bar length to 28%
     progressBar = '28%'
@@ -152,11 +145,11 @@ function question4() {
     // very similar approach to question 3
 
     // hide question 3 element, show question 4 element
-    question3.style.display = 'none'
-    question4.style.display = 'block'
+    question_3.style.display = 'none'
+    question_4.style.display = 'block'
     
     // variable assignment
-    let alcoholRadioSelect = document.getElementById('alcohol-radio') // alcohol yes-no radio select
+    let alcoholRadioSelect = document.getElementsByName('alcohol-radio') // alcohol yes-no radio select
     let alcoholAmountRadioForm = document.getElementById('alcohol-amount') // alcohol amount radio buttons in fieldset
 
     // update progress-bar length to 42%
@@ -197,11 +190,11 @@ function question4() {
 // coffee is a placeholder for a hot drink - tea, hot chocolate, etc
 function question5() {
     // hide question 4 element, show question 5 element
-    question4.style.display = 'none'
-    question5.style.display = 'block'
+    question_4.style.display = 'none'
+    question_5.style.display = 'block'
 
     // variable assignment
-    let coffeeRadioSelect = document.getElementById('coffee-radio') // coffee yes-no radio selector
+    let coffeeRadioSelect = document.getElementsByName('coffee-radio') // coffee yes-no radio selector
     let coffeeAmountRadioForm = document.getElementById('coffee-amount')
 
     // update progressBar to 56%
@@ -239,12 +232,12 @@ function question5() {
 
 function queston6() {
     // hide question 5 element, show question 6 element
-    question5.style.display = 'none'
-    question6.style.display = 'block'
+    question_5.style.display = 'none'
+    question_6.style.display = 'block'
 
     // variable assignment
-    let clothingRadioSelect = document.getElementById('clothing-radio') // coffee yes-no radio selector
-    let clothingAmountRadioForm = document.getElementById('clothing-amount')
+    let clothingRadioSelect = document.getElementsByName('clothes-radio') // coffee yes-no radio selector
+    let clothingAmountRadioForm = document.getElementById('clothes-amount')
 
     // update progressBar to 70%
     progressBar = '70%'
@@ -281,8 +274,8 @@ function queston6() {
 
 function question7() {
     // hide question 6 element, show question 7 element
-    question6.style.display = 'none'
-    question7.style.display = 'block'
+    question_6.style.display = 'none'
+    question_7.style.display = 'block'
 
     // variable assignment
     let lotteryRadioSelect = document.getElementById('lottery-radio') // lottery yes-no radio selector
@@ -337,7 +330,7 @@ function question7() {
 function final() {
 
     // hide question 7 element, show final output
-    question7.style.display = 'none'
+    question_7.style.display = 'none'
     finalOutput.style.display = 'block'
 
     // update progressBar to 100%
